a1319a45e712b5a4ffc55ccf75aeeed017f47abf
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-08-28 21:36:28 +0300

    models

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -10,7 +10,6 @@ const usersSchema = new Schema(
     },
     name: {
       type: String,
-      required: true,
     },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,11 +1,9 @@
 import Joi from 'joi';
-
-export const loginUserSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
-});
-
 export const registerUserSchema = Joi.object({
+  email: Joi.string().email().min(3).max(30).required(),
+  password: Joi.string().min(8).max(64).required(),
+});
+export const loginUserSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
